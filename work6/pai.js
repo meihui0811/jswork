ebaf1859cf7d194c94540d88024d4517774041b8
meihui0811 <13144473128@163.com>
2019-10-16 15:45:34 +0800

    wokr6

---
+++ work6/pai.js
@@ -0,0 +1,23 @@
+function calculate(){
+    let num = document.getElementById('').value
+    num = parseInt(num)&&Number(num)
+    if(!isNaN(num)){
+        document.getElementById(''.value = pi( ))
+    }
+}
+function factorial(n){
+    let sum = 1
+    for(let i= ){
+
+    }
+    return sum
+}
+function pi(num){
+    let sum = 0
+    for(let i=0;i<=num;++i){
+        let dividend = factorial( )
+        let divisor = oddFactorial( )
+
+    }
+    return sum
+}
