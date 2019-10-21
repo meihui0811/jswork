5a21edaaecc5ee22ae07decf1ffe8fc352d59478
meihui0811 <56482297+meihui0811@users.noreply.github.com>
2019-10-21 19:45:16 +0800

    Update utils.js

--- work7/utils.js
+++ work7/utils.js
@@ -1,7 +1,7 @@
 function bubbleSort(str) {
     let arr = str.split(',')
     for (let i = 0; i < arr.length - 1; i++) {
-        for (let i = 0; j< arr.length - i - 1; j++) {
+        for (let j = 0; j< arr.length - i - 1; j++) {
             if (arr[j] >arr[j + 1]) {
                 [arr[j], arr[j + 1]] = [arr[j + 1], arr[j]]
 
@@ -12,10 +12,10 @@ function bubbleSort(str) {
 }
 function insertSort(str) {
     let arr = str.split(',')
-    for (let i = 1; i < aee.length; i++){
-        for (let j = i; j > 0; j--){
+    for (let i = 1; i < arr.length; i++){
+        for (let j = i; j > 0; j--) {
         if (arr[j - 1] > arr[j]) {
-            [arr[j - 1],arr[j]] = [arr[j]], arr[j - 1]
+            [arr[j - 1], arr[j]] = [arr[j], arr[j - 1]
         }
         }
     }
